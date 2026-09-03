9ac8f69025859081002661ad2148c4d45b55c8c3
Alina <gazievaalina070792@gmail.com>
2026-09-03 18:44:08 +0300

    Task 1 is fixed
    Task 1 is done

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,16 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Task 1");
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Task 1");//Задача 1
+        int clientIOS = 1;
+    if (clientIOS == 0) {
+        System.out.println("Установите версию приложения для iOS по ссылке.");
+    } else if (clientIOS == 1) {
+        System.out.println("Установите версию приложения для Android по ссылке.");
+    } else {
+        System.out.println("Операционная система не поддержвается");
+    }
+        System.out.println("Task 2");//Задача 2
+        System.out.println("Task 3");//Задача 4
     }
 }
