b7c3379165a25db6166a959d3ef71caa883bc59c
Alina <gazievaalina070792@gmail.com>
2026-09-03 19:05:26 +0300

    Task 2 is done

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,30 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Task 1");//Задача 1
-        int clientIOS = 1;
-    if (clientIOS == 0) {
+        int clientOS = 1;
+    if (clientOS == 0) {
         System.out.println("Установите версию приложения для iOS по ссылке.");
-    } else if (clientIOS == 1) {
+    } else if (clientOS == 1) {
         System.out.println("Установите версию приложения для Android по ссылке.");
     } else {
-        System.out.println("Операционная система не поддержвается");
+        System.out.println("Операционная система не поддержвается.");
     }
         System.out.println("Task 2");//Задача 2
+        int clientDeviceYear = 2022;
+        if (clientOS == 1){
+            if (clientDeviceYear < 2015){
+                System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+            } else {
+                System.out.println("Установите обычную версию приложения для Android по ссылке.");
+            }
+        } else {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            } else {
+                System.out.println("Установите обычную версию приложения для iOS по ссылке");
+            }
+        }
+
         System.out.println("Task 3");//Задача 4
     }
 }
